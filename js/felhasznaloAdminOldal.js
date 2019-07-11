8b4a97128846b00d227218111d4f1059e0267670
Juhasz Dorottya <juhasz.dossz@gmail.com>
2019-07-11 17:34:06 +0200

    legalapabb szerkezet

---
+++ js/felhasznaloAdminOldal.js
@@ -0,0 +1,32 @@
+var User = {
+  data: [],
+  //   init() {
+  //     this.getData();
+  //   },
+  getData() {
+    var request = new XMLHttpRequest();
+    // a this itt az xmlhttprequest lenne, ezért arrow function-nal kell írni, mert az nem bind-onja a this-t
+    request.onreadystatechange = () => {
+      if (request.readyState === 4 && request.status === 200) {
+        this.callback(request.responseText);
+      }
+    };
+    request.open('GET', '/data/users.json');
+    request.send();
+  },
+  // ezzel töltöm fel a data tömböt, ha a válasz megérkezett
+  callback(jsonContent) {
+    this.data = JSON.parse(jsonContent).users;
+    this.showAll();
+  },
+  showAll() {
+    console.log(this.data); // ez ír bele a html-be
+  },
+  remove() {},
+  create() {},
+  stored() {}
+
+};
+User.getData();
+
+
