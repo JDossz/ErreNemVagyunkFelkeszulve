fb9713c6879e4fb6d9108bd59a2c2c4210a157c9
Rebeka Grosics <rebeka.grosics@gmail.com>
2019-07-12 11:13:27 +0200

    showAll megírva

--- js/felhasznaloAdminOldal.js
+++ js/felhasznaloAdminOldal.js
@@ -11,7 +11,7 @@ var User = {
         this.callback(request.responseText);
       }
     };
-    request.open('GET', '/data/users.json');
+    request.open('GET', '../data/users.json');
     request.send();
   },
   // ezzel töltöm fel a data tömböt, ha a válasz megérkezett
@@ -21,6 +21,19 @@ var User = {
   },
   showAll() {
     console.log(this.data); // ez ír bele a html-be
+
+    var nodeTBody = document.querySelector('.usersTable');
+    //nodeTBody.textContent = '';
+
+    for (var i = 0; i < this.data.length; i += 1) {
+      var nodeTr = document.createElement('tr');
+      for (var membername in this.data[i]) {
+        var nodeTd = document.createElement('td');
+        nodeTd.textContent = this.data[i][membername];
+        nodeTr.appendChild(nodeTd);
+      }
+      nodeTBody.appendChild(nodeTr);
+    }
   },
   remove() {},
   create() {},
@@ -28,5 +41,3 @@ var User = {
 
 };
 User.getData();
-
-
