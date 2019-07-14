7e2162dafcb9a49337afc77a96343687127b4822
Oláh Dániel <olah.daniel95@gmail.com>
2019-07-14 08:59:17 +0200

    Bug has been found, rewriteing ~all

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -1,66 +1,42 @@
 var EventHandler = {
   records: [],
-  editors: [],
   tmpName: "",
   tmpMail: "",
   tmpAddress: "",
   addEditButtonListeners() {
-    this.editors = document.querySelectorAll('.button--edit');
-    console.log(this.editors);
-    console.log(this.editors.length);
-    for (let i = 0; i < this.editors.length; i += 1) {
-      this.editors[i].addEventListener('click', this.editButtonEvent);
-    }
+    $(document).ready(function () {
+      $('.button--edit').click(function () {
+        let btn = event.currentTarget;
+        EventHandler.removeEditButtonListeners();
+        EventHandler.editButtonEvent(btn);
+      });
+    });
   },
-  editButtonEvent() {
-    let row = event.target;
-    while (row.nodeName !== 'TR') {
-      row = row.parentNode;
-    }
-    this.removeEditors();
-    this.switchToInput(row.children[1]);
-    this.switchToInput(row.children[2]);
-    this.switchToInput(row.children[3]);
-    this.switchToSave(row.children[4]);
-    this.switchToCancel(row.children[5]);
+  editButtonEvent(clicked) {
+    
   },
   saveButtonEvent() {
-    let btn = event.target;
-    btn.addEventListener('click', saveClicked);
+
   },
   cancelButtonEvent() {
-    let row = event.target;
-    while (row.nodeName !== 'TR') {
-      row = row.parentNode;
-    }
-    row.children[1].innerHTML = this.tmpName;
-    row.children[2].innerHTML = this.tmpMail;
-    row.children[3].innerHTML = this.tmpAddress;
-    this.addEditors();
+
   },
   removeEditButtonListeners() {
-    for (let i = 0; i < this.editors.length; i++) {
-      this.editors[i].removeEventListener('click', this.edit);
-    }
+    $(document).ready(function () {
+      $('.button--edit').off('click');
+    });
   },
   switchToInput(cell) {
-    let temp = text;
-    cell.innerHTML = '<input type="text" class="text--edited">';
-    cell.children[0].value = temp;
+
   },
   switchToSave(cell) {
-    cell.textContent = '';
-    cell.textContent = 'Mentés';
-    cell.addEventListener('click', saveButtonEvent);
+
   },
   switchToCancel(cell) {
-    cell.textContent = '';
-    cell.textContent = 'Visszavonás';
-    cell.addEventListener('click', this.cancelButtonEvent);
+
   },
   saveClicked() {
-    // ANIMÁCIÓ MAJD
-    this.addEditors();
+
   }
 };
 
