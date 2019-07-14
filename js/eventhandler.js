94a9cece4c9b2d53fd3211ad13d9c2880cf55854
Oláh Dániel <olah.daniel95@gmail.com>
2019-07-14 09:20:43 +0200

    Szerkesztés gomb már működik

--- js/eventhandler.js
+++ js/eventhandler.js
@@ -12,8 +12,24 @@ var EventHandler = {
       });
     });
   },
-  editButtonEvent(clicked) {
-    
+  editButtonEvent(row) {
+    while (row.nodeName !== 'TR') {
+      row = row.parentNode;
+    }
+    let name = row.children[1];
+    let mail = row.children[2];
+    let address = row.children[3];
+    row.children[4].addEventListener('click', this.saveButtonEvent);
+    row.children[5].addEventListener('click', this.cancelButtonEvent);
+    EventHandler.tmpName = name.innerHTML;
+    EventHandler.tmpMail = mail.innerHTML;
+    EventHandler.tmpAddress = address.innerHTML;
+    name.innerHTML = '<input type="text" class="input--edit">';
+    name.children[0].value = this.tmpName;
+    mail.innerHTML = '<input type="text" class="input--edit">';
+    mail.children[0].value = this.tmpMail
+    address.innerHTML = '<input type="text" class="input--edit">';
+    address.children[0].value = this.tmpAddress;
   },
   saveButtonEvent() {
 
