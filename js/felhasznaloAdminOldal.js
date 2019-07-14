ca336d9a9d77de308f0a3c316e6bdd6958c7906b
Horvath Szilvia <horvath.szilvia.ildiko@gmail.com>
2019-07-14 16:26:35 +0200

    JS, CSS, HTML módosítás

--- js/felhasznaloAdminOldal.js
+++ js/felhasznaloAdminOldal.js
@@ -70,3 +70,20 @@ var User = {
 
 };
 User.getData();
+
+
+// +szilvi
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
+function inputResetButton() {
+  document.getElementById('registerForm').reset();
+}
+// +szilvi vége
