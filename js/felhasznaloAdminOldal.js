618baecf49604a2aa38b27845a51962d16b138c7
Horvath Szilvia <horvath.szilvia.ildiko@gmail.com>
2019-07-14 21:27:56 +0200

    felhasznaloAdminOldal css, js, html újra feltéve, az én kódom még nincsbenne

--- js/felhasznaloAdminOldal.js
+++ js/felhasznaloAdminOldal.js
@@ -8,7 +8,7 @@ var User = {
         this.callback(request.responseText);
       }
     };
-    request.open('GET', '/data/users.json');
+    request.open('GET', '../data/users.json');
     request.send();
   },
 
@@ -17,74 +17,63 @@ var User = {
     this.showAll();
   },
   showAll() {
-    // ez ír bele a html-be
+    // console.log(this.data); // ez ír bele a html-be
+
+    var nodeTBody = document.querySelector('.usersTable>tbody');
+    nodeTBody.textContent = '';
+
+    for (var i = 0; i < this.data.length; i += 1) {
+      var nodeTr = document.createElement('tr');
+      for (var membername in this.data[i]) {
+        var nodeTd = document.createElement('td');
+        nodeTd.textContent = this.data[i][membername];
+        nodeTr.appendChild(nodeTd);
+      }
+      var nodeButtonEdit = document.createElement('button');
+      nodeButtonEdit.textContent = '✎ Szerkesztés';
+      nodeButtonEdit.setAttribute('class', 'button--edit');
+      nodeButtonEdit.addEventListener('click', EventHandler.editButtonEvent);
+      var nodeButtonDel = document.createElement('button');
+      nodeButtonDel.textContent = '❌ Törlés';
+      nodeButtonDel.setAttribute('class', 'button--del');
+      nodeButtonDel.addEventListener('click', this.remove);
+      nodeTr.appendChild(nodeButtonEdit);
+      nodeTr.appendChild(nodeButtonDel);
+      nodeTBody.appendChild(nodeTr);
+    }
   },
-  remove() {},
-  create() {},
-  stored() {},
+  remove() {
+    var nodeButtonDel = event.target;
+    var nodeTr = nodeButtonDel.parentNode;
+    var userID = parseInt(nodeTr.firstElementChild.innerHTML);
+    // console.log(userID);
+    var user;
 
-  registerUser() {
-    var largestNumber = 0;
-    this.data.forEach(el => {
-      if (el.id > largestNumber) {
-        largestNumber = el.id;
-      }
-    });
-    var registerUserId = largestNumber + 1;
-    var registerUserName = document.getElementById('registerUserName').value;
-    var registerUserEmailAddress = document.getElementById('registerUserEmailAddress').value;
-    var registerUserAddress = document.getElementById('registerUserAddress').value;
-    var newUser = {
-      id: registerUserId,
-      name: registerUserName,
-      emailAddress: registerUserEmailAddress,
-      address: registerUserAddress
-    };
-    var isItNew = true;
-    for (var i = 0; i < this.data.length; i++) {
-      // ellenőrizzük, ha a név már létezik
-      if (registerUserName === this.data[i].name) {
-        // figyelmeztetés, ha a név foglalt
-        alert('Ez a név foglalt, kérlek adj meg egy másikat.');
-        isItNew = false;
-        // álljon meg, ha true
-        break;
-        // ellenőrizzük, hogy 1-nél rövoidebb karaktert adtak e meg
-      } else if ((registerUserName.length < 2) || (registerUserEmailAddress.length < 2) || (registerUserAddress.length < 1)) {
-        // figyelmeztetés, ha a név rövidebb, mint 1 karakter
-        alert('A név túl rövid, kérlek adj meg egy másikat.');
-        isItNew = false;
-        // álljon meg, ha true
+    user = (getUserById(userID));
+    for (var i = 0; i < User.data.length; i++) {
+      if (user === User.data[i]) {
+        User.data.splice(i, 1);
         break;
       }
-
-      if (isItNew) {
-        this.data.push(newUser);
-      }
-      console.log(this.data);
-      // Szilvi vége
     }
 
     User.showAll();
     console.log(this.data);
-  }
+  },
+  create() {},
+  stored() {}
 
 };
 User.getData();
 
-function inputResetButton() {
-  document.getElementById('registerForm').reset();
-}
+function getUserById(userID) {
+  var result = null;
 
-// +szilvi
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
+  for (var i = 0; i < User.data.length; i++) {
+    if (userID === User.data[i].id) {
+      result = User.data[i];
+      break;
+    }
   }
   return result;
 }
-;
