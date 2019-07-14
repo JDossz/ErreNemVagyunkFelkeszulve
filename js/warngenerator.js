f717d9298bc2d48a36c6a440a97dee90ebc59b2f
Oláh Dániel <olah.daniel95@gmail.com>
2019-07-14 16:09:37 +0200

    Felugróblak kész

--- js/warngenerator.js
+++ js/warngenerator.js
@@ -1,23 +1,37 @@
 var WarnGenerator = {
   deleteWarn() {
     let field = document.getElementById('felugroablak');
-    emptyClassList(field);
+    WarnGenerator.emptyClassList(field);
     field.classList.add('red-warn');
     field.classList.add('felugro');
     field.innerHTML = 'Elem törlése sikeres!';
+    setTimeout(function () {
+      WarnGenerator.emptyClassList(field);
+    }, 3000);
   },
   cancelWarn() {
     let field = document.getElementById('felugroablak');
-    emptyClassList(field);
+    WarnGenerator.emptyClassList(field);
     field.classList.add('yellow-warn');
     field.classList.add('felugro');
     field.innerHTML = 'Elem szerkesztése visszavonva!';
+    setTimeout(function () {
+      WarnGenerator.emptyClassList(field);
+    }, 3000);
   },
   successWarn() {
     let field = document.getElementById('felugroablak');
-    emptyClassList(field);
+    WarnGenerator.emptyClassList(field);
     field.classList.add('green-warn');
     field.classList.add('felugro');
     field.innerHTML = 'Elem szerkesztése sikeres!';
+    setTimeout(function () {
+      WarnGenerator.emptyClassList(field);
+    }, 3000);
   },
+  emptyClassList(field) {
+    for (let i = 0; i < field.classList.length; i += 1) {
+      field.classList.remove(field.classList[i]);
+    }
+  }
 };
